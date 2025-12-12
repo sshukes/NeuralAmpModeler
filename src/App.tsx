198ba0d55b3fb1e88bd83e6cab6d9998f00db37c
sshukes <sshukes@msn.com>
2025-12-12 11:01:04 -0500

    Theme metadata dialog to neon console style

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import RunDetailsPage from './pages/RunDetailsPage';
 import NamTrainerPage from './pages/NamTrainerPage';
 import RunListPage from './pages/RunListPage';
+import ThemeTestPage from './pages/ThemeTestPage';
 function App() {
   return (
     <Routes>
       <Route path="/" element={<NamTrainerPage />} />
       <Route path="/runs" element={<RunListPage />} />
       <Route path="/runs/:id" element={<RunDetailsPage />} />
+      <Route path="/theme-test" element={<ThemeTestPage />} />
       {/* add other routes here */}
     </Routes>
   );
